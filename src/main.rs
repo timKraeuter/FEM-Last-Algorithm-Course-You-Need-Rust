8c7a50ec432f596511f52cf6bbc5b30f2772de91
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-03-26 11:29:47 +0100

    Add random problem picking

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod problems;
 mod solutions;
+use rand::seq::SliceRandom;
 
 fn main() {
-    println!("This is just a placeholder");
+    let problem_names = vec![
+        "linear_search",
+        "binary_search",
+        "two_crystal_balls",
+        "bubble_sort",
+    ];
+    println!("All problems are {:?}.", problem_names);
+    println!();
+
+    let mut rng = rand::thread_rng();
+    println!(
+        "Try to implement {} in the problems directory.",
+        problem_names.choose(&mut rng).unwrap()
+    );
 }
